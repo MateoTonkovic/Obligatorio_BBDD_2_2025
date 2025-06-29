e83f1b6f37a114c6408ca74a785f9163cca05f2c
ClaraPlateiro <clara.plateiro@correo.ucu.edu.uy>
2025-06-29 18:51:19 -0300

    avances

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const cors = require('cors')
-const seeder = require('./seeder');
-const db = require('./db');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-
 app.use(express.json());
+
+const listasRouter = require("./routes/listas");
+const votosRouter = require("./routes/votos");
+
+app.use('/api/listas', listasRouter);
+app.use('/api/votos', votosRouter);
 
 app.get('/', (req, res) => {
     res.send({ message: 'mensaje de backend' });
 });
-app.use('/api', seeder);
+
+const seeder = require('./seeder');
 
 app.listen(PORT, () => {
     console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import EmisionVoto from "./Views/EmisionVoto/EmisionVoto";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    const [backendMsg, setBackendMsg] = useState('Cargando…');
+//   const [backendMsg, setBackendMsg] = useState("Cargando…");
 
-    useEffect(() => {
-        fetch('http://localhost:3001/')
-            .then((res) => {
-                if (!res.ok) throw new Error(`HTTP error ${res.status}`);
-                return res.json();
-            })
-            .then((data) => {
-                console.log('data', data);
-                setBackendMsg(data.message);
-            })
-            .catch((err) => {
-                console.error('Error al llamar al backend:', err);
-                setBackendMsg('Error al conectar con el backend');
-            });
-    }, []);
+//   useEffect(() => {
+//     fetch("http://localhost:3001/")
+//       .then((res) => {
+//         if (!res.ok) throw new Error(`HTTP error ${res.status}`);
+//         return res.json();
+//       })
+//       .then((data) => {
+//         console.log("data", data);
+//         setBackendMsg(data.message);
+//       })
+//       .catch((err) => {
+//         console.error("Error al llamar al backend:", err);
+//         setBackendMsg("Error al conectar con el backend");
+//       });
+//   }, []);
 
-    return (
-        <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-            <h1>Front</h1>
-            <p>
-                {backendMsg}
-            </p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/votar" element={<EmisionVoto />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
